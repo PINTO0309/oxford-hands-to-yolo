1403140a431ea2794d55359a81e14b9d0abd203b
pinto0309 <rmsdh122@yahoo.co.jp>
2022-09-13 19:22:33 +0900

    update

--- draw_box.py
+++ draw_box.py
@@ -1,7 +1,10 @@
-#coding:utf-8
+#!/usr/bin/env python
+
 import cv2
 import os
+import sys
 import random
+import argparse
 
 
 label_folder = 'hand_dataset/training_dataset/training_data/new_annotations/'
@@ -16,24 +19,21 @@ def plot_one_box(x, image, color=None, label=None, line_thickness=None):
     tl = line_thickness or round(0.002 * (image.shape[0] + image.shape[1]) / 2) + 1  # line/font thickness
     color = color or [random.randint(0, 255) for _ in range(3)]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-    cv2.rectangle(image, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    cv2.rectangle(image, c1, c2, color, thickness=1, lineType=cv2.LINE_AA)
     if label:
         tf = max(tl - 1, 1)  # font thickness
         t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
         c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-        cv2.rectangle(image, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.rectangle(image, c1, c2, color, -1, cv2.LINE_AA)
         cv2.putText(image, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
 
 
-def draw_box_on_image(image_name, classes, colors, label_folder, raw_images_folder, save_images_folder ):
-    txt_path  = os.path.join(label_folder,'%s.txt'%(image_name))
-    print(image_name)
-    if image_name == '.DS_Store':
-        return 0
-    image_path = os.path.join( raw_images_folder,'%s.jpg'%(image_name))
-
-    save_file_path = os.path.join(save_images_folder,'%s.jpg'%(image_name))
-
+def draw_box_on_image(
+    image_path,
+    txt_path,
+    classes,
+    colors,
+):
     # flag_people_or_car_data = 0
     source_file = open(txt_path)
     image = cv2.imread(image_path)
@@ -62,18 +62,14 @@ def draw_box_on_image(image_name, classes, colors, label_folder, raw_images_fold
         #     cv2.imwrite(save_file_path,draw_car_tangle)
 
         plot_one_box([x1,y1,x2,y2], image, color=colors[class_idx], label=classes[class_idx], line_thickness=None)
-
-        cv2.imwrite(save_file_path,image)
-
         box_number += 1
-    return box_number
+
+    return box_number, image
 
 
 
 def make_name_list(raw_images_folder, name_list_path):
-
     image_file_list = os.listdir(raw_images_folder)
-
     text_image_name_list_file=open(name_list_path,'w')
 
     for  image_file_name in image_file_list:
@@ -84,19 +80,51 @@ def make_name_list(raw_images_folder, name_list_path):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-v',
+        '--enable_image_view',
+        action='store_true',
+    )
+    parser.add_argument(
+        '-s',
+        '--enable_image_save',
+        action='store_true',
+    )
+    args = parser.parse_args()
+    enable_image_view = args.enable_image_view
+    enable_image_save = args.enable_image_save
+    if not enable_image_view and not enable_image_save:
+        print('Either enable_image_view or enable_image_save must be specified.')
+        sys.exit(0)
 
     make_name_list(raw_images_folder, name_list_path)
-
     classes = image_names = open(classes_path).read().strip().split()
     random.seed(42)
     colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(classes))]
-
     image_names = open(name_list_path).read().strip().split()
-
     box_total = 0
     image_total = 0
     for image_name in image_names:
-        box_num = draw_box_on_image(image_name, classes, colors, label_folder, raw_images_folder, save_images_folder)
+        txt_path  = os.path.join(label_folder, f'{image_name}.txt'%())
+        image_path = os.path.join( raw_images_folder, f'{image_name}.jpg')
+        save_file_path = os.path.join(save_images_folder, f'{image_name}.jpg')
+        box_num, image = draw_box_on_image(
+            image_path,
+            txt_path,
+            classes,
+            colors,
+        )
         box_total += box_num
         image_total += 1
-        print('Box number:', box_total, 'Image number:',image_total)
+        print(f'image_path: {image_path} Box number: {box_total} Image number: {image_total}')
+
+        if enable_image_save:
+            cv2.imwrite(save_file_path, image)
+
+        if enable_image_view:
+            cv2.imshow('Viewer', image)
+
+        key = cv2.waitKey(0)
+        if key == 27:  # ESC
+            break
